4348adf4bf1b8045421780b8210be114ebb0ecca
Manuel Paniagua <manuel.paniagua84@gmail.com>
2018-02-06 00:00:46 -0800

    update prod domain

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -7,7 +7,7 @@ import { tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
-  domain: string = 'http://localhost:8080';   // development domain
+  domain: string = '';   // development domain
   authToken: string;
   user: string;
   options: RequestOptions;
